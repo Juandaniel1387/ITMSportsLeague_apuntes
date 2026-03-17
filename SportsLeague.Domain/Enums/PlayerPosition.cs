b5833894d3dd27af4007691036a6cc1869df1348
Juan Daniel Velásquez López <sistemas6@igbcolombia.com>
2026-03-17 12:50:10 -0500

    Fase 2 completada

---
+++ SportsLeague.Domain/Enums/PlayerPosition.cs
@@ -0,0 +1,17 @@
+﻿namespace SportsLeague.Domain.Enums;
+
+
+public enum PlayerPosition
+
+{
+
+    Goalkeeper = 0,
+
+    Defender = 1,
+
+    Midfielder = 2,
+
+    Forward = 3
+
+}
+  //Sirve para registrar valores que quiero guardar en la base de datos en forma de ID para evitar errores de ingreso en la base de datos
